59367d5d99beedadb99da623d27de4f72fdff9ad
dikidikay <dikidikayy@gmail.com>
2026-05-02 10:31:32 +0700

    first commit

---
+++ app/routes/layouts/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Hero from "~/components/Hero";
+const HomeLayout = () => {
+  return (
+    <>
+      <Hero name="Dicky" />
+      <section className="max-w-6xl mx-auto px-6 my-8">
+        <Outlet />
+      </section>
+    </>
+  );
+};
+
+export default HomeLayout;
